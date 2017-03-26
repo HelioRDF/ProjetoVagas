df28ad8d6c73b7d83326cfca81ca550bfba63130
HelioRDF <jpfranca@bol.com.br>
2017-03-26 00:32:08 -0300

    Avaliação de desempenho,
    Avaliação iniciada entre Listener, Before e After...
    Necessário avaliar melhor o ciclo de vida

--- src/main/java/br/com/projetovagas/util/AutenticacaoListener.java
+++ src/main/java/br/com/projetovagas/util/AutenticacaoListener.java
@@ -12,10 +12,13 @@ import br.com.projetovagas.domain.usuarios.Usuario;
 public class AutenticacaoListener implements PhaseListener {
 
 	private static final long serialVersionUID = 5370242150329490062L;
+	long tempoInicial = System.currentTimeMillis(); //Captura o tempo inicial da execução da Classe
 
 	@Override
 	public void afterPhase(PhaseEvent event) {
-
+		System.out.println("\n-----------------------");
+		
+		
 		String paginaAtual = Faces.getViewId();
 		boolean paginaDeLogin = paginaAtual.contains("login.xhtml");
 		LoginBean loginBean =  new LoginBean();
@@ -49,19 +52,27 @@ public class AutenticacaoListener implements PhaseListener {
 		System.out.println("LoginBean:" + loginBean);
 		System.out.println("Página Atual:" + paginaAtual);
 		System.out.println("Admin:"+usuario.getAdmin());
-
+		
+		long tempoAfter = System.currentTimeMillis(); //Captura o tempo final da execução da Classe
+		System.out.printf("\n Tempo de execução: %.3f ms%n", (tempoAfter - tempoInicial) / 1000d); //Imprime o tempo de execução da classe em Ms.
+	
 
 	}
 
 	@Override
 	public void beforePhase(PhaseEvent event) {
+		System.out.println("\n-----------------------");
+		tempoInicial = System.currentTimeMillis(); //Captura o tempo inicial da execução da Classe
 		System.out.println("BeforePhase:" + event.getPhaseId());
+		long tempoBefore = System.currentTimeMillis(); //Captura o tempo final da execução da Classe
+		System.out.printf("\n Tempo de execução: %.3f ms%n", (tempoBefore - tempoInicial) / 1000d); //Imprime o tempo de execução da classe em Ms.
+		
 
 	}
 
 	@Override
 	public PhaseId getPhaseId() {
-		// TODO Auto-generated method stub
+	
 		return PhaseId.RESTORE_VIEW;
 	}
 
